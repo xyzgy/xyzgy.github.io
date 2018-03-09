d69200cbddef48379c0befd3c78a6339cc750a05
xyzgy <577816250@qq.com>
2018-03-09 13:33:27 +0800

    xc

--- other/css/nvestor.css
+++
@@ -1,6 +0,0 @@
-// 首页事件
-$('.work_ul li .work_title').click(function() {
-
-
-
-})
--- other/js/news.js
+++ other/js/news.js
@@ -1,7 +1,9 @@
 $('.hyzx').click(function() {
+	let index = $(this).index();
+
 	$('.page_show .siblings_page').hide();
 	$('.gsdt_page').hide();
-	$('.hyzx_page').show();
+	$('.hyzx_page').eq(index).show();
 
 })
 
